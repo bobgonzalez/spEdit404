a060b64fe300f0ebacbf05e3c174c1bd04b6bb88
Robert Gonzalez <labratjazzcat@users.noreply.github.com>
2017-06-04 19:19:11 -0400

    Add files via upload

---
+++ spEdit.py
@@ -0,0 +1,153 @@
+from pattern import *
+
+'''
+TODO: use redis or shelve to let user save patterns
+TODO: e - edit hex values
+TODO: V - change all velocities
+TODO: D* - delete all occurrences of one pad, V* - change all velocities of one pad
+TODO: add swing function
+TODO: add triplet function
+TODO: move note by ticks
+TODO: copy subsection of pattern
+TODO: merge two patterns
+TODO: REFACTOR
+TODO: add more try, except blocks to prevent crash on wrong input
+'''
+
+
+def menu():
+    ret = "r : read pattern from binary\n" + "i : insert note\n" + "d : delete note\nD : clear all notes\n" + "v : change note velocities\n" + "h : change note length\n" + "l : set pattern length\n" + "m : double pattern\n" + "w : write pattern to binary\n" + "x : exit\n"
+    return ret
+
+if not os.path.exists("./export"):
+    os.makedirs("./export")
+if not os.path.exists("./import"):
+    os.makedirs("./import")
+length = raw_input('enter length of pattern in bars or enter \'r\' to load pattern >')
+if length != 'r':
+    length = int(length)
+    p = Pattern(length)
+else:
+    p = Pattern(1)
+    bank = raw_input('enter bank > ')
+    pad = raw_input('enter pad > ')
+    try:
+        p.read_pattern(bank, pad)
+    except:
+        print("error reading binary : error 202")
+    print(p.print_pattern())
+while True:
+    print(menu())
+    usr_in = raw_input('> ')
+    try:
+        if usr_in == 'a':
+            b = raw_input('enter bank > ')
+            pad = int(raw_input('enter pad # > '))
+            v = int(raw_input('enter velocity 0-127 > '))
+            le = int(raw_input('enter length > '))
+            n = int(raw_input('enter next note start > '))
+            try:
+                n4 = Note(b, pad, v, le, n, p.time)
+            except:
+                print("error creating note : 101")
+            try:
+                p.add_note(n4,len(p.score)-1, len(p.score[len(p.score)-1].notes))
+            except:
+                print("error adding note : error 301")
+        elif usr_in == 'pr':
+            print(p.print_pattern())
+        elif usr_in == "p":
+            print("0 : 1-2-3-4 \n1 : 4 on the floor \n")
+            selection = raw_input('enter bank > ')
+            if selection == '0':
+                for i in range(p.length):
+                    n1 = Note('D', 12, 127, 60, 96, 0+(384*i))
+                    p.add_note(n1, 0, (4*i)+0)
+                    n3 = Note('D', 11, 127, 60, 96, 96+(384*i))
+                    p.add_note(n3, 0, (4*i)+1)
+                    n2 = Note('D', 9, 127, 60, 96, 192+(384*i))
+                    p.add_note(n2, 0, (4*i)+2)
+                    n4 = Note('D', 10, 127, 60, 96, 288+(384*i))
+                    p.add_note(n4, 0, (4*i)+3)
+            elif selection == '1':
+                for i in range(p.length * 4):
+                    n1 = Note('D', 12, 127, 60, 96, 0+(i*96))
+                    p.add_note(n1, 0, 0+i)
+            print(p.print_pattern())
+        elif usr_in == 'e':
+            break
+        elif usr_in == 'w':
+            bank = raw_input('enter bank > ')
+            pad = raw_input('enter pad > ')
+            try:
+                p.write_binary(bank, pad)
+            except:
+                print("error writing binary : error 203")
+        elif usr_in == 'r':
+            bank = raw_input('enter bank > ')
+            pad = raw_input('enter pad > ')
+            try:
+                p.read_pattern(bank, pad)
+            except:
+                print("error reading binary : error 202")
+            print(p.print_pattern())
+        elif usr_in == 'l':
+            p.length = int(raw_input('enter number of bars > '))
+            p.ticks = p.length * 384
+            print(p.print_pattern())
+        elif usr_in == 'd':
+            bank = raw_input('enter bank > ')
+            pad = raw_input('enter pad > ')
+            bn = int(raw_input('enter bar # > '))
+            n = int(raw_input('enter location x : x/16 > '))
+            try:
+                p.delete_note(bank, pad, bn, n)
+            except:
+                print("error deleting note : error 305")
+            print(p.print_pattern())
+        elif usr_in == 'D':
+            p.clear_notes()
+            print(p.print_pattern())
+        elif usr_in == 'v':
+            bank = raw_input('enter bank > ')
+            pad = raw_input('enter pad > ')
+            bn = int(raw_input('enter bar # > '))
+            n = int(raw_input('enter location x : x/16 > '))
+            v = int(raw_input('enter new velocity 1- 127 > '))
+            try:
+                p.change_note_velocity(bank, pad, bn, n, v)
+            except:
+                print("error changing note velocity : error 306")
+            print(p.print_pattern())
+        elif usr_in == 'i':
+            b = raw_input('enter bank > ')
+            pad = int(raw_input('enter pad # > '))
+            v = int(raw_input('enter velocity 0-127 > '))
+            le = int(raw_input('enter length > '))
+            bn = int(raw_input('enter bar # > '))
+            n = int(raw_input('enter location x : x/16 > '))
+            try:
+                n4 = Note(b, pad, v, le, n + (bn - 1) * 16, int(n) * 24)
+            except:
+                print("error creating note : 101")
+            try:
+                p.add_note_at(n4, bn - 1, n)
+            except:
+                print("error adding note : error 302")
+            print(p.print_pattern())
+        elif usr_in == 'm':
+            p.double()
+            print(p.print_pattern())
+        elif usr_in == 'h':
+            bank = raw_input('enter bank > ')
+            pad = raw_input('enter pad > ')
+            bn = int(raw_input('enter bar # > '))
+            n = int(raw_input('enter location x : x/16 > '))
+            v = int(raw_input('enter new length (384 ticks per bar) > '))
+            try:
+                p.change_note_length(bank, pad, bn, n, v)
+            except:
+                print("error changing note length : error 307")
+            print(p.print_pattern())
+    except:
+        print("error reading input : error 201")
