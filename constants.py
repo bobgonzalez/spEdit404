90c13430875860c26672392d6ba33377713ca7b3
Robert <2234jr@gmail.com>
2018-12-12 09:03:33 -0600

    added constants module and timer object to track bpm and millisec per tick

---
+++ constants.py
@@ -0,0 +1,7 @@
+ticks_per_bar = 384
+millisec_per_min = 60000
+ascii_character_offset = 97
+number_of_bank_pads = 4
+secondary_bank_offset = 5
+pads_per_bank = 12
+pad_offset_magic_number = 46
---
+++ timer.py
@@ -0,0 +1,8 @@
+import constants
+
+
+class Timer():
+
+    def __init__(self, bpm):
+        self.bpm = bpm
+        self.milli_per_tick = ((constants.millisec_per_min/self.bpm)/constants.ticks_per_bar)*4
