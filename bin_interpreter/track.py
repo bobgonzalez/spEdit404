8b0a1634254c8a3194aa6b386b99aa2cf70a0a1d
Robert <2234jr@gmail.com>
2019-08-03 21:20:30 -0500

    refactored constants

--- bin_interpreter/track.py
+++ bin_interpreter/track.py
@@ -1,5 +1,5 @@
 import copy
-
+from . import constants
 
 class Pattern:
     def __init__(self, length):
@@ -10,7 +10,7 @@ class Pattern:
         new_pattern = Pattern(len(self)+len(other))
         notes_to_add = copy.deepcopy(other.notes)
         for note in notes_to_add:
-            note.time_start = note.time_start + (self.length*384)
+            note.time_start = note.time_start + (self.length*constants.ticks_per_bar)
         new_pattern.notes = sorted(self.notes+other.notes, key=lambda n: n.time_start)
         return new_pattern
 
@@ -27,21 +27,21 @@ class Pattern:
 
 class Note:
     def __init__(self, pad, bank, start_tick, length, velocity):
-        if 0 < pad <= 12 and type(start_tick) == int:
+        if 0 < pad <= constants.pads_per_bank and type(start_tick) == int:
             self.pad = pad
         else:
-            raise ValueError("pad must be integer between 1-12")
+            raise ValueError(f'pad must be integer between 1-{constants.pads_per_bank}')
         # TODO write bank validation
         self.bank = bank
         if start_tick >= 0 and type(start_tick) == int:
             self.start_tick = start_tick
         else:
-            raise ValueError("start_tick must be a positive integer")
+            raise ValueError('start_tick must be a positive integer')
         if length >= 0 and type(length) == int:
             self.length = length
         else:
-            raise ValueError("length must be a positive integer")
-        if 0 < velocity <= 127 and type(start_tick) == int:
+            raise ValueError('length must be a positive integer')
+        if 0 < velocity <= constants.max_velocity and type(start_tick) == int:
             self.velocity = velocity
         else:
-            raise ValueError("velocity must be integer between 1-127")
+            raise ValueError(f'velocity must be integer between 1-{constants.max_velocity}')
