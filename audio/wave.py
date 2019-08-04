2d25c6e296a13d147231dbdbb28bc2819d1aca48
Robert <2234jr@gmail.com>
2019-08-03 21:37:27 -0500

    started preview pattern to play back pattern based on new class structure

---
+++ audio/wave.py
@@ -0,0 +1,24 @@
+from pydub import AudioSegment
+from bin_interpreter import constants
+
+
+def preview_pattern(pattern, bpm):
+    MpT = milliseconds_per_tick(bpm)
+    silence = AudioSegment.from_file("/home/robert/PycharmProjects/spEdit404/silence.wav")
+    s1 = silence[:MpT]
+    ret = s1[:1]
+    for bar in pattern.score:
+        for note in bar.notes:
+            print(note.path)
+            s = s1 * note.start_tick
+            pd = str(note.pad)
+            if len(pd) == 1:
+                pd = "0" + pd
+            path = "/home/robert/PycharmProjects/spEdit404/SP-404SX/SMPL/" + note.bank.upper() + "00000" + pd + ".WAV"
+            a = AudioSegment.from_file(path)
+            ret.overlay(s + a[:note.length * MpT])
+    ret.play()
+
+
+def milliseconds_per_tick(bpm):
+    return ((constants.millisec_per_min/bpm)/constants.ticks_per_bar)*4
