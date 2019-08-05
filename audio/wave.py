59fdc62e0c96186e93496615792f301999c07340
Robert <2234jr@gmail.com>
2019-08-04 19:13:16 -0500

    updated to new pydub api

--- audio/wave.py
+++ audio/wave.py
@@ -1,23 +1,19 @@
 from pydub import AudioSegment
+from pydub.playback import play
 from bin_interpreter import constants
 
 
 def preview_pattern(pattern, bpm):
     MpT = milliseconds_per_tick(bpm)
-    silence = AudioSegment.from_file("./silence.wav")
-    s1 = silence[:MpT]
-    ret = s1[:1]
-    for bar in pattern.score:
-        for note in bar.notes:
-            print(note.path)
-            s = s1 * note.start_tick
-            pd = str(note.pad)
-            if len(pd) == 1:
-                pd = "0" + pd
-            path = "./SP-404SX/SMPL/" + note.bank.upper() + "00000" + pd + ".WAV"
-            a = AudioSegment.from_file(path)
-            ret.overlay(s + a[:note.length * MpT])
-    ret.play()
+    pattern_audio = AudioSegment.silent(duration=constants.ticks_per_bar * pattern.length * MpT)
+    for note in pattern.notes:
+        pd = str(note.pad)
+        if len(pd) == 1:
+            pd = "0" + pd
+        path = "./SP-404SX/SMPL/" + note.bank.upper() + "00000" + pd + ".WAV"
+        sample_audio = AudioSegment.from_file(path)
+        pattern_audio = pattern_audio.overlay(sample_audio[:note.length * MpT], position=note.start_tick)
+    play(pattern_audio)
 
 
 def milliseconds_per_tick(bpm):
