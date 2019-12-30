4b6f6bb71e972467c497adb491c4af4910efdc92
Robert <2234jr@gmail.com>
2019-12-30 00:19:46 -0600

    add logger to create folder method

--- utils.py
+++ utils.py
@@ -1,4 +1,9 @@
 import os
+import logging
+
+
+logger = logging.getLogger()
+
 
 def remove_file(file_path):
 	if os.path.exists(file_path):
@@ -6,8 +11,12 @@ def remove_file(file_path):
 
 
 def create_folder(folder_path):
-	if not os.path.exists(folder_path):
-		os.makedirs(folder_path)
+	try:
+		if not os.path.exists(folder_path):
+			os.makedirs(folder_path)
+	except Exception:
+		logger.exception(f'could not create {folder_path}')
+
 
 def add_padding(padee, length):
 	padee = str(padee)
