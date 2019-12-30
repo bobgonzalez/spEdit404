4a1bb03a7f2649c5d9bc6d6dd9ceac29831891e6
Robert <2234jr@gmail.com>
2019-12-30 00:19:22 -0600

    extract main action loop

--- spEdit.py
+++ spEdit.py
@@ -20,35 +20,38 @@ def get_user_input(fields):
     return values
 
 
+def action_loop(pattern, menu_choice):
+    if menu_choice == 'a':
+        bank, pad, velocity, length, start_tick = get_user_input(['bank', 'pad', 'velocity 0-127',
+                                                                  'length', f'note start 0-{384 * len(pattern)}> '])
+        new_note = Note(pad, bank, start_tick, length, velocity)
+        pattern.add_note(new_note)
+    elif menu_choice == 'w':
+        bank, pad = get_user_input(['bank', 'pad'])
+        write_binary(pattern, bank, pad)
+    elif menu_choice == 'r':
+        bank, pad = get_user_input(['bank', 'pad'])
+        pattern = read_pattern(bank, pad)
+    elif menu_choice == 'l':
+        pattern.change_length = int(input('enter number of bars > '))
+    elif menu_choice == 'd':
+        track, note = get_user_input(['track', 'note'])
+        pattern.delete_note(track, note)
+    elif menu_choice == 'p':
+        bpm = get_user_input(['bpm'])
+        preview_pattern(pattern, bpm)
+    return pattern
+
+
 if __name__ == "__main__":
     setup_folders()
-    length = input('enter length of pattern or enter \'r\' to load pattern >')
-    if length == 'r':
+    menu_choice = input('enter length of pattern or enter \'r\' to load pattern >')
+    if menu_choice == 'r':
         bank, pad = get_user_input(['bank', 'pad'])
         pattern = read_pattern(bank, pad)
     else:
-        pattern = Pattern(int(length))
-    while True:
+        pattern = Pattern(int(menu_choice))
+    while menu_choice != 'x':
         print(pattern)
         menu_choice = menu()
-        if menu_choice == 'a':
-            bank, pad, velocity, length, start_tick = get_user_input(['bank', 'pad', 'velocity 0-127',
-                                                                      'length', f'note start 0-{384 * len(pattern)}> '])
-            new_note = Note(pad, bank, start_tick, length, velocity)
-            pattern.add_note(new_note)
-        elif menu_choice == 'w':
-            bank, pad = get_user_input(['bank', 'pad'])
-            write_binary(pattern, bank, pad)
-        elif menu_choice == 'r':
-            bank, pad = get_user_input(['bank', 'pad'])
-            pattern = read_pattern(bank, pad)
-        elif menu_choice == 'l':
-            pattern.change_length = int(input('enter number of bars > '))
-        elif menu_choice == 'd':
-            track, note = get_user_input(['track', 'note'])
-            pattern.delete_note(track, note)
-        elif menu_choice == 'p':
-            bpm = get_user_input(['bpm'])
-            preview_pattern(pattern, bpm)
-        elif menu_choice == 'x':
-            break
+        pattern = action_loop(pattern, menu_choice)
