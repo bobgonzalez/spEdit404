4da5000747793b65a128ed8abccdd8af80a9260b
Robert <2234jr@gmail.com>
2019-12-29 22:07:19 -0600

    clean up try / except blocks and rename variables

--- spedit404/spEdit.py
+++ spedit404/spEdit.py
@@ -16,76 +16,47 @@ def setup_folders():
     create_folder("./tmp")
 
 
-
 if __name__ == "__main__": 
     setup_folders()
     length = input('enter length of pattern in bars or enter \'r\' to load pattern >')
     if length != 'r':
         length = int(length)
-        p = Pattern(length)
+        pattern = Pattern(length)
     else:
         bank = input('enter bank > ')
         pad = input('enter pad > ')
-        try:
-            p = read_pattern(bank, pad)
-        except Exception as e:
-            e.printStackTrace()
-            print("error reading binary : error 202")
-        print(p)
+        pattern = read_pattern(bank, pad)
+        print(pattern)
     while True:
         print(menu())
         usr_in = input('> ')
-        try:
-            if usr_in == 'a':
-                b = input('enter bank > ')
-                pad = int(input('enter pad # > '))
-                v = int(input('enter velocity 0-127 > '))
-                le = int(input('enter length > '))
-                n = int(input(f'enter note start 0-{384*len(p)}> '))
-                try:
-                    n4 = Note(pad, b, n, le, v)
-                except Exception as e:
-                    e.printStackTrace()
-                    print("error creating note : 101")
-                try:
-                    p.add_note(n4)
-                except Exception as e:
-                    e.printStackTrace()
-                    print("error adding note : error 301")
-                print(p)
-            elif usr_in == 'pr':
-                print(p)
-            elif usr_in == 'w':
-                bank = input('enter bank > ')
-                pad = input('enter pad > ')
-                try:
-                    write_binary(p, bank, pad)
-                except Exception as e:
-                    e.printStackTrace()
-                    print("error writing binary : error 203")
-            elif usr_in == 'r':
-                bank = input('enter bank > ')
-                pad = input('enter pad > ')
-                try:
-                    p = read_pattern(bank, pad)
-                    print(p)
-                except Exception as e:
-                    e.printStackTrace()
-                    print("error reading binary : error 202")
-            elif usr_in == 'l':
-                p.change_length = int(input('enter number of bars > '))
-                print(p)
-            elif usr_in == 'd':
-                track = input('enter track # 0-11 > ')
-                note = input('enter note # 0-n > ')
-                try:
-                    p.delete_note(track, note)
-                except Exception as e:
-                    e.printStackTrace()
-                    print("error deleting note : error 305")
-                print(p)
-            elif usr_in == 'x':
-                break
-        except Exception as e:
-            e.printStackTrace()
-            print("error reading input : error 201")
+        if usr_in == 'a':
+            bank = input('enter bank > ')
+            pad = int(input('enter pad # > '))
+            velocity = int(input('enter velocity 0-127 > '))
+            length = int(input('enter length > '))
+            start_tick = int(input(f'enter note start 0-{384 * len(pattern)}> '))
+            new_note = Note(pad, bank, start_tick, length, velocity)
+            pattern.add_note(new_note)
+            print(pattern)
+        elif usr_in == 'pr':
+            print(pattern)
+        elif usr_in == 'w':
+            bank = input('enter bank > ')
+            pad = input('enter pad > ')
+            write_binary(pattern, bank, pad)
+        elif usr_in == 'r':
+            bank = input('enter bank > ')
+            pad = input('enter pad > ')
+            pattern = read_pattern(bank, pad)
+            print(pattern)
+        elif usr_in == 'l':
+            pattern.change_length = int(input('enter number of bars > '))
+            print(pattern)
+        elif usr_in == 'd':
+            track = input('enter track # 0-11 > ')
+            note = input('enter note # 0-n > ')
+            pattern.delete_note(track, note)
+            print(pattern)
+        elif usr_in == 'x':
+            break
